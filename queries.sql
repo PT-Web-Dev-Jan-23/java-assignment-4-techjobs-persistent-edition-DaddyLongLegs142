fdf88fc96c5e8e19c8a7c26ea3d23ecf6dd4135e
Ben Spiecker <blspiecker95@gmail.com>
2023-06-06 17:21:01 -0500

    Made Employer and Skills Persistent

--- queries.sql
+++ queries.sql
@@ -1,6 +1,13 @@
 -- Part 1: Test it with SQL
+--create table job(
+--    id integer,
+--    name varchar,
+--    employer varchar,
+--    skills varchar,
+--)
 
 -- Part 2: Test it with SQL
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
