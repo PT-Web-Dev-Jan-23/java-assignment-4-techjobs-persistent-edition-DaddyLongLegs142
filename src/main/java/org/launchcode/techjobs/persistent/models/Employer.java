badae3fb31847d2fd651191a03c1f8bb18423bfa
Ben Spiecker <blspiecker95@gmail.com>
2023-06-11 15:41:47 -0500

    Set up ORM for Job and Employer

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -2,8 +2,12 @@ package org.launchcode.techjobs.persistent.models;
 
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,6 +15,10 @@ public class Employer extends AbstractEntity {
     @NotBlank
     @Size(min = 3, max = 100)
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() {}
 
